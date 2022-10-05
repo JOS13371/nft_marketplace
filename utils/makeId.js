e64f4809b86698344a397ffdf015995a51986601
JOS13371 <jos133711@gmail.com>
2022-10-05 14:38:49 +0200

    Add Best Creator

---
+++ utils/makeId.js
@@ -0,0 +1,11 @@
+export const makeId = (Length) => {
+  let result = '';
+  const charracters = 'abcdefghijklmnopqrstuvwxyz123456789';
+  const charractersLength = charracters.length;
+
+  for (let i = 0; i < Length; i++) {
+    result += charracters.charAt(Math.floor(Math.random() * charractersLength));
+  }
+  return result;
+};
+
