82026a9b6d1441e02e0bbdf215d3d441394b7012
JOS13371 <jos133711@gmail.com>
2022-10-07 20:21:00 +0200

    Create All Context application need

---
+++ context/NFTContext.js
@@ -0,0 +1,154 @@
+import React, { useState, useEffect } from 'react';
+import Web3Modal from 'web3modal';
+import { ethers } from 'ethers';
+import axios from 'axios';
+import { create as ipfsHttpClient } from 'ipfs-http-client';
+import { MarketAddress, MarketAddressABI } from './constants';
+
+const fetchContract = (signerOrProvider) => new ethers.Contract(MarketAddress, MarketAddressABI, signerOrProvider);
+
+export const NFTContext = React.createContext();
+
+// process.env.NEXT_PUBLIC_IPFS_PROJECT_ID;
+const projectId = '2FlsoynP1REu4EOKbXBdK9ND2om';
+// process.env.NEXT_PUBLIC_API_KEY_SECRET
+const projectSecret = '4ddb9e0ac9b5fce4ec4338f7f054c204';
+const auth = `Basic ${Buffer.from(`${projectId}:${projectSecret}`).toString('base64')}`;
+const subdomain = 'https://nftmarketplace1337.infura-ipfs.io';
+
+const client = ipfsHttpClient({
+  host: 'ipfs.infura.io',
+  port: 5001,
+  protocol: 'https',
+  headers: {
+    authorization: auth,
+  },
+});
+
+export const NFTProvider = ({ children }) => {
+  const [currentAccount, setCurrentAccount] = useState('');
+  const nftCurrency = 'ETH';
+
+  const checkIfWalletIsConnected = async () => {
+    if (!window.ethereum) return alert('Please install MetaMask');
+
+    const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length) {
+      setCurrentAccount(accounts[0]);
+    } else {
+      console.log('No accounts found.');
+    }
+  };
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  const connectWallet = async () => {
+    if (!window.ethereum) return alert('Please install MetaMask');
+
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+    setCurrentAccount(accounts[0]);
+
+    window.location.reload();
+  };
+
+  const createNFT = async (formInput, fileUrl, router) => {
+    const { name, description, price } = formInput;
+
+    if (!name || !description || !price || !fileUrl) return;
+
+    const data = JSON.stringify({ name, description, image: fileUrl });
+
+    try {
+      const added = await client.add(data);
+      const URL = `${subdomain}/ipfs/${added.path}`;
+
+      await createSale(URL, price);
+
+      router.push('/');
+    } catch (error) {
+      console.log('Error uploading file: ', error);
+    }
+  };
+
+  const createSale = async (URL, formInputPrice, isReselling, id) => {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+
+    const price = ethers.utils.parseUnits(formInputPrice, 'ether');
+    const contract = fetchContract(signer);
+    const listingPrice = await contract.getListingPrice();
+
+    const transaction = await contract.createToken(URL, price, { value: listingPrice.toString() });
+
+    await transaction.wait();
+  };
+
+  const fetchNFTs = async () => {
+    const provider = new ethers.providers.JsonRpcProvider();
+    const contract = fetchContract(provider);
+
+    const data = await contract.fetchMarketItems();
+
+    const items = await Promise.all(data.map(async ({ tokenId, seller, owner, price: unformattedPrice }) => {
+      const tokenURI = await contract.tokenURI(tokenId);
+      const { data: { image, name, description } } = await axios.get(tokenURI);
+      const price = ethers.utils.formatUnits(unformattedPrice.toString(), 'ether');
+
+      return {
+        price,
+        tokenId: tokenId.toNumber(),
+        seller,
+        owner,
+        image,
+        name,
+        description,
+        tokenURI,
+      };
+    }));
+    return items;
+  };
+
+  const fetchMyNFTsOrListedNFTs = async (type) => {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+
+    const contract = fetchContract(signer);
+
+    const data = type === 'fetchItemsListed'
+
+      ? await contract.fetchItemsListed()
+      : await contract.fetchMyNFTs();
+
+    const items = await Promise.all(data.map(async ({ tokenId, seller, owner, price: unformattedPrice }) => {
+      const tokenURI = await contract.tokenURI(tokenId);
+      const { data: { image, name, description } } = await axios.get(tokenURI);
+      const price = ethers.utils.formatUnits(unformattedPrice.toString(), 'ether');
+
+      return {
+        price,
+        tokenId: tokenId.toNumber(),
+        seller,
+        owner,
+        image,
+        name,
+        description,
+        tokenURI,
+      };
+    }));
+
+    return items;
+  };
+
+  return (
+    <NFTContext.Provider value={{ nftCurrency, connectWallet, currentAccount, client, subdomain, createNFT, fetchNFTs, fetchMyNFTsOrListedNFTs }}>
+      {children}
+    </NFTContext.Provider>
+  );
+};
